9d6902973c92503966fbf44fb9be2e98d3a5b51e
Hilliard Stamper <hilliard.stamper@att.net>
2022-03-06 17:28:54 -0500

    added homepage data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+// used to serve the react-front end code
+const path = require('path');
+
 // import the middleware function
 const { authMiddleware } = require('./utils/auth');
 
@@ -35,6 +38,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
